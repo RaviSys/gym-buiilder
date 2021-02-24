f2c2679a3033fae7a7bf02f8906becc29d3ef5df
Ravi Singh <ravionrails@gmail.com>
2021-02-24 23:43:24 +0530

    quick fix syntax error

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -3,7 +3,7 @@ class Plan < ApplicationRecord
   validates :name, :price, :duration, presence: true
 
   # Association
-  has_many :member_plans. dependent: :destroy
+  has_many :member_plans, dependent: :destroy
 
   # Instance Methods
   def amount
