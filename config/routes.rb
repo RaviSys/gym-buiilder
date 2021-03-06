abe68b789e8ec1313582635a556fee845d7765f5
Ravi Singh <ravionrails@gmail.com>
2021-03-06 13:08:57 +0530

    added templates for errors pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
   root 'home#index'
   devise_for :admins, skip: [:registrations] 
   resources :contacts
